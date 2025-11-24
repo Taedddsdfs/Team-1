065c02f3d6b1cdf74c96e4107a0f960cd84844fa
Fyttt7 <ft624@ic.ac.uk>
2025-11-24 18:26:45 +0000

    Add files via upload

--- RTL/registerfile.sv
+++ RTL/registerfile.sv
@@ -1,6 +1,6 @@
-module registerfile(
+module registerfile (
     input logic          clk,
-    input logic          rst
+    input logic          rst,
     input logic          WE3,
     input logic   [4:0]  AD1,
     input logic   [4:0]  AD2,
@@ -8,27 +8,23 @@ module registerfile(
     input logic  [31:0]  WD3,
     output logic [31:0]  RD1,
     output logic [31:0]  RD2,
-    output logic [31:0]  a0,
+    output logic [31:0]  a0
 );
 
 logic [31:0] registers [31:0];
 
-initial begin
-    for(int i = 0; i < 32; i++){
-        registers[i] = 32'h0;
-    }
-end;
-
-always_ff @(posedge clk)begin
-    if(rst)begin
-        for(int i = 0; i < 32; i++){
-        registers[i] = 32'h0;
-    }
-    end;
-    else if(WE3 && (AD3 != 5'b0))begin
-        registers[AD3] <= WD3;
-    end;
-end;
+always_ff @(posedge clk) begin
+        if (rst) begin
+            int i;
+            for (i = 0; i < 32; i++) begin
+                registers[i] <= 32'h0000_0000;
+            end
+        end else begin
+            if (WE3 && (AD3 != 5'd0)) begin
+                registers[AD3] <= WD3;
+            end
+        end
+    end
 
     assign RD1 = (AD1 == 5'b0) ? 32'h0 : registers[AD1];
     assign RD2 = (AD2 == 5'b0) ? 32'h0 : registers[AD2];
