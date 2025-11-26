2288adf2df51485ae22fc2b9460097ec95f92c29
Fyttt7 <ft624@ic.ac.uk>
2025-11-26 20:02:05 +0000

    Add files via upload

--- RTL/registerfile.sv
+++ RTL/registerfile.sv
@@ -1,33 +1,30 @@
-module registerfile (
+module registerfile #(
+    parameter ADDRESS_WIDTH = 5,
+              DATA_WIDTH = 32
+) (
     input logic          clk,
     input logic          rst,
     input logic          WE3,
-    input logic   [4:0]  AD1,
-    input logic   [4:0]  AD2,
-    input logic   [4:0]  AD3,
-    input logic  [31:0]  WD3,
-    output logic [31:0]  RD1,
-    output logic [31:0]  RD2,
-    output logic [31:0]  a0
+    input logic   [ADDRESS_WIDTH-1:0]  AD1,
+    input logic   [ADDRESS_WIDTH-1:0]  AD2,
+    input logic   [ADDRESS_WIDTH-1:0]  AD3,
+    input logic  [DATA_WIDTH:0]  WD3,
+    output logic [DATA_WIDTH:0]  RD1,
+    output logic [DATA_WIDTH:0]  RD2,
+    output logic [DATA_WIDTH:0]  a0
 );
 
-logic [31:0] registers [31:0];
+logic [DATA_WIDTH:0] registers [2**ADDRESS_WIDTH-1:0];
 
-always_ff @(posedge clk) begin
-        if (rst) begin
-            int i;
-            for (i = 0; i < 32; i++) begin
-                registers[i] <= 32'h0000_0000;
-            end
-        end else begin
-            if (WE3 && (AD3 != 5'd0)) begin
-                registers[AD3] <= WD3;
-            end
-        end
-    end
+assign registers[0] = 0;
 
-    assign RD1 = (AD1 == 5'b0) ? 32'h0 : registers[AD1];
-    assign RD2 = (AD2 == 5'b0) ? 32'h0 : registers[AD2];
-    assign a0 = registers[10];
+always_comb begin
+    a0 = registers[10];
+    RD1 = registers[AD1];
+    RD2 = registers[AD2];
+end
+
+always_ff @(posedge clk)
+            if (WE3 && (AD3 != 5'd0))       registers[AD3] <= WD3;
 
 endmodule
