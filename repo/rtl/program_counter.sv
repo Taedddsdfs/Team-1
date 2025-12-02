7a3d19f014a31ca568a62bb751f98579cde50f84
JingtingHuang-alb <alberthuangjt@gmail.com>
2025-12-02 15:35:20 +0000

    Refactor program counter to handle multiple PCsrc states

--- repo/rtl/program_counter.sv
+++ repo/rtl/program_counter.sv
@@ -1,21 +1,31 @@
 module program_counter #(
     parameter WIDTH = 32
 )(
-    input  logic                 clk,
-    input  logic                 rst,
-    input  logic                 PCsrc,
-    input  logic [31:0]          ImmOp,
-    output logic [WIDTH-1:0]     PC
+    input  logic             clk,
+    input  logic             rst,
+    
+    // three states
+    input  logic [1:0]       PCsrc,      
+    
+    // JAL (PC + Imm)
+    input  logic [31:0]      ImmOp,      
+    
+    // JALR 
+    input  logic [31:0]      ALUResult,  
+    
+    output logic [WIDTH-1:0] PC
 );
 
     always_ff @(posedge clk) begin
         if (rst) begin
             PC <= 32'h0;
         end else begin
-            if (PCsrc)
-                PC <= PC + ImmOp;  // Branch/Jump
-            else
-                PC <= PC + 32'd4;  // Normal increment
+            case (PCsrc)
+                2'b00: PC <= PC + 32'd4;      // Normal (Next Instr)
+                2'b01: PC <= PC + ImmOp;      // Branch / JAL (PC + Imm)
+                2'b10: PC <= ALUResult;       // JALR (Rs1 + Imm) 
+                default: PC <= PC + 32'd4;    
+            endcase
         end
     end
 
