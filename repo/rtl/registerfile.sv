0653f86fd36c5deeb6ee1a87a45892e29a648e85
Taedddsdfs <115664141+Taedddsdfs@users.noreply.github.com>
2025-12-12 21:06:17 +0000

    Update registerfile.sv

--- repo/rtl/registerfile.sv
+++ repo/rtl/registerfile.sv
@@ -12,11 +12,12 @@ module registerfile #(
     output logic [DATA_WIDTH-1:0]  RD2,
     output logic [DATA_WIDTH-1:0]  a0
 );
-
+// Array of 32 registers, each 32-bit wide
 logic [DATA_WIDTH-1:0] registers [2**ADDRESS_WIDTH-1:0];
 
 assign registers[0] = 0;
-
+// Combinational Logic for Reading
+// RISC-V registers are read asynchronously.
 always_comb begin
     a0 = registers[10];
     RD1 = registers[AD1];
@@ -25,5 +26,7 @@ end
 
 always_ff @(posedge clk)
     if (WE3 && (AD3 != 5'd0))       registers[AD3] <= WD3;
-
+// Note: Depending on the synthesizer, explicit assignment 
+    // to registers[0] might be needed if read logic doesn't handle x0 explicitly.
+    // However, the write condition (AD3 != 0) effectively protects x0.
 endmodule
